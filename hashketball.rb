2ca4d52dde86e888f78bcb99b55fb3e05900fc46
Reese M Bowman <reesemb1@gmail.com>
2020-01-09 00:35:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -257,8 +257,9 @@ def winning_team()
     while index < game_hash[team][:players].length
       team_points += game_hash[team][:players][index][:points]
       current_team = game_hash[team][:team_name]
+      index += 1 
     end
-    
+    binding.pry
   end
   
 end
