2cc4a016202bba66646a1311ce09ee2c5686b320
Reese M Bowman <reesemb1@gmail.com>
2020-01-09 00:54:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -272,10 +272,9 @@ def player_with_longest_name()
   }
   
   game_hash.each do |team, data|
-    game_hash[team][:player].each do |player, stat|
-      binding.pry
+    binding.pry
     
-    end
+    
   end
 end
 
